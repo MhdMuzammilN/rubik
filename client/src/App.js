bbcbc1ffc2f4f21ac4b5fb70b304a9a556db4de2
MhdMuzammilN <muzammil06n@gmail.com>
2023-09-19 22:55:28 +0530

    NavBar just got an update --mzml.exe

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import HomePage from "./pages/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
 import ServicePage from "./pages/ServicePage";
-import NewFile from "./pages/NewFile";
+
 function App() {
   return (
     <Router>
@@ -12,7 +12,6 @@ function App() {
         <Route path="/" element={<HomePage />}>
           <Route path="admin" element={<AdminPage />} />
           <Route path="service" element={<ServicePage />} />
-          <Route path="new" element={<NewFile/>}/>
         </Route>
       </Routes>
     </Router>
