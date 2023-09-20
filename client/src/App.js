bf1c6d55c595d34885e9c7b91a5cdefec8297fa4
binoy barnabas <binoybarnabas@gmail.com>
2023-09-20 17:36:00 +0530

    muz should work on navbar fix, outlet is broken

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import HomePage from "./pages/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
 import ServicePage from "./pages/ServicePage";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Router>
-      <Routes>
+      <NavBar />
+        <Routes>
         <Route path="/" element={<HomePage />}>
           <Route path="admin" element={<AdminPage />} />
           <Route path="service" element={<ServicePage />} />
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,31 @@
-import NavBar from "../components/NavBar";
-import { Outlet } from "react-router-dom";
+import { Carousel } from "@material-tailwind/react";
+import e1 from '../assets/e1.jpg'
+import e2 from '../assets/e2.jpg'
+import e3 from '../assets/e3.jpg'
+
 const HomePage = () => {
-    return ( 
-        <div>
-            <NavBar />
-            <Outlet />
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <Carousel className="rounded-xl" transition={{ duration: 1 }}>
+        <img
+          src={e1}
+          alt="image 1"
+          className="h-full w-full object-cover"
+        />
+        <img
+          src={e2}
+          alt="image 2"
+          className="h-full w-full object-cover"
+        />
+        <img
+          src={e3}
+          alt="image 3"
+          className="h-full w-full object-cover"
+        />
+      </Carousel>
+     
+    </div>
+  );
+};
+
 export default HomePage;
